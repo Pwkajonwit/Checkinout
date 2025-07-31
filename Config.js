28ee05c52ef5a959c29723952b0e0eabf276825d
Pwkajonwit <powertec.civil@gmail.com>
2025-07-31 19:39:43 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,11 +1,11 @@
 // ประกาศค่า 
 
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-const WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const WEB_APP_OT_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
-const APPROVED_WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwznaltyZR6jwxETn9pDEAws81KfMZ9xmtN7CLJgFeuTlx0i0daGM1VMSxknl1thg7wvg/exec';
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyYiZVStG89ftVAmuURXw2Tk3T_-QFXtP3b1l_b4URI6E4ZS_8wbZKGiQkzr91tn2ygIA/exec';
+const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbySAJvMvLj0wXMVpBLCKWP9P_h_tSiMpLmAYBmKaG9kB3BFAV2FpJmGFmtvyjdFDdSGpA/exec';
+const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbwj6-J3VxLIiJTCStERpa71lKCS9SUaeiq9yaR8twAx88xggD26Z07nSezptkfhoqDR/exec';
+const APPROVED_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxT_c4nupr5HwusOzNB_mzLpGFMM2EqEcrtZ1gUTxw8Fi-Qwe9MAHQjq1O8aLEj6EW2WQ/exec';
 
 //LIFF_ID
 const LIFF_ID = 'XXXXXXXXX'; // LIFF ID Checkin
