7cd98deb2edcc64a164b2f07a7b050bcef37700f
Pwkajonwit <powertec.civil@gmail.com>
2025-11-22 11:25:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ประกาศค่า 
 
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwznaltyZR6jwxETn9pDEAws81KfMZ9xmtN7CLJgFeuTlx0i0daGM1VMSxknl1thg7wvg/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxF7vugRVlSq759UM3_MttwQEUO60AiVKmcfHL7aEfiCBl_hJiqEfkkp7ZwN8KhkCGZxQ/exec';
 const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyYiZVStG89ftVAmuURXw2Tk3T_-QFXtP3b1l_b4URI6E4ZS_8wbZKGiQkzr91tn2ygIA/exec';
 const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbySAJvMvLj0wXMVpBLCKWP9P_h_tSiMpLmAYBmKaG9kB3BFAV2FpJmGFmtvyjdFDdSGpA/exec';
 const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbwj6-J3VxLIiJTCStERpa71lKCS9SUaeiq9yaR8twAx88xggD26Z07nSezptkfhoqDR/exec';
@@ -13,3 +13,4 @@ const LIFF_ID_HISTORY = '2007858091-em61p9Nz'; // LIFF ID History
 const LIFF_ID_LEAVE = '2007858091-e6OGk7nD'; // LIFF ID Leave
 const LIFF_ID_OT = '2007858091-0Qw7RpAv'; // LIFF ID Ot
 const LIFF_ID_REGISTER = '2007858091-qyGM1JAV'; // LIFF ID Register
+
